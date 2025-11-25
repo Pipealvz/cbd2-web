8c33faa79b25fd96d5fd3375bdf94069362743f7
Pipealvz <pipedim03@gmail.com>
2025-11-24 21:37:17 -0500

    Updating gitignore

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ function incrementCount() {
     count += 1;
     return count;
 }
+
+console.log(incrementCount()); // Output: 1
+console.log(incrementCount() + 1); // Output: +1 // Output: 3
+for (let i = 0; i < 2; i++) {
+    console.log(incrementCount(), "Final count", count); // Output: 4, Output: 5
+}
